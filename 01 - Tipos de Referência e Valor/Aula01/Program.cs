82302a7ddb6b4e64191a2007ecffd0d372ca13ef
Isabella Melo <isacpmelo@gmail.com>
2022-04-25 23:47:26 -0300

    Atualizão do conteúdo

--- 01 - Tipos de Referência e Valor/Aula01/Program.cs
+++ 01 - Tipos de Referência e Valor/Aula01/Program.cs
@@ -1,6 +1,23 @@
 ﻿using static System.Console;
 public class Program
 {
+    static void Demo6()
+    {
+       int[] numeros = new int[] {0,2,4,6,8};
+       WriteLine($"Digite aqui o número que gostaria de encontrar");
+       var numero = int.Parse(ReadLine());
+       var idxEncontrado = EncontrarNumero(numeros, numero);
+
+       if (idxEncontrado >= 0) 
+       {
+            WriteLine($"O número encontrado está na posição {idxEncontrado}");
+       }
+       else 
+       {
+           WriteLine("O número digitado não foi encontrado.");
+       }
+    }
+
     static void Demo5()
     {
         int[] pares = new int[]{0,2,4,6,8};
@@ -94,21 +111,42 @@ public class Program
         }
         return - 1;
     }
+
+    static bool EncontrarPessoa(List<Pessoa> pessoas, Pessoa pessoa)
+    {
+        foreach (var item in pessoas)
+        {
+            if(item.Nome == pessoa.Nome)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void Main()
     {
-       int[] numeros = new int[] {0,2,4,6,8};
-       WriteLine($"Digite aqui o número que gostaria de encontrar");
-       var numero = int.Parse(ReadLine());
-       var idxEncontrado = EncontrarNumero(numeros, numero);
+        List<Pessoa> pessoas = new List<Pessoa>()
+        {
+            new Pessoa(){Nome = "Isabella"},
+            new Pessoa(){Nome = "Pitty"},
+            new Pessoa(){Nome = "Romeu"},
+            new Pessoa(){Nome = "Julieta"},
+            new Pessoa(){Nome = "Baratinha"},
+        };
 
-       if (idxEncontrado >= 0) 
-       {
-            WriteLine($"O número encontrado está na posição {idxEncontrado}");
-       }
-       else 
-       {
-           WriteLine("O número digitado não foi encontrado.");
-       }
-      
+        WriteLine("Digite a pessoa que gostaria de localizar: ");
+
+        var nome = ReadLine();
+        var pessoa = new Pessoa(){Nome = nome};
+        var encontrado = EncontrarPessoa(pessoas, pessoa);
+
+        if(encontrado)
+        {
+            WriteLine("Pessoa Localizada");
+        }
+        else
+        {
+            WriteLine("Pessoa não localizada");
+        }
     }
 }
